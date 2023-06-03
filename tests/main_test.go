9123d399b6f281e533f144aef065f6c041e68aa2
Shamistan <shemistan1@gmail.com>
2023-06-03 16:53:44 +0300

    test

---
+++ tests/main_test.go
@@ -0,0 +1,27 @@
+package tests
+
+import (
+	"github.com/Edazila/test/lesson1/task1"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestRun(t *testing.T) {
+	t.Run("1 + 2", func(t *testing.T) {
+		res := task1.RunSum(1, 2)
+		expected := 3
+		assert.Equal(t, expected, res)
+	})
+
+	t.Run("0 + 0", func(t *testing.T) {
+		res := task1.RunSum(0, 0)
+		expected := 0
+		assert.Equal(t, expected, res)
+	})
+
+	t.Run("-1 + -1", func(t *testing.T) {
+		res := task1.RunSum(-1, -1)
+		expected := -2
+		assert.Equal(t, expected, res)
+	})
+}
